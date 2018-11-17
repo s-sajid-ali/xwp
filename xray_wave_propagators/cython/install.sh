41df7b81dec517624f5b25887cd192060aa85a65
s-sajid-ali <sajidsyed2021@u.northwestern.edu>
2018-11-17 15:17:07 -0600

    modified:   xray_wave_propagators/cython/install.sh

--- xray_wave_propagators/cython/install.sh
+++ xray_wave_propagators/cython/install.sh
@@ -1,20 +1,17 @@
-module load intel18
-spack load miniconda3
-source activate intelpy3
 export CYTHON_HOME=$PWD
 cd $CYTHON_HOME/1d
 rm *.c
-LDSHARED="icc -shared" CC=icc python setup.py build_ext --inplace
+python setup.py build_ext --inplace
 cp *.so $CYTHON_HOME/binaries
 cd $CYTHON_HOME/2d/2_loop
 rm *.c
-LDSHARED="icc -shared" CC=icc python setup.py build_ext --inplace
+python setup.py build_ext --inplace
 cp *.so $CYTHON_HOME/binaries
 cd $CYTHON_HOME/2d/4_loop/serial
 rm *.c
-LDSHARED="icc -shared" CC=icc python setup.py build_ext --inplace
+python setup.py build_ext --inplace
 cp *.so $CYTHON_HOME/binaries
 cd $CYTHON_HOME/2d/4_loop/parallel
 rm *.c
-LDSHARED="icc -shared" CC=icc python setup.py build_ext --inplace
+python setup.py build_ext --inplace
 cp *.so $CYTHON_HOME/binaries
