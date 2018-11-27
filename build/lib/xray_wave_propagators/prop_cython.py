253721dcc76a119fccfbb30216b8c3a19b17af04
s-sajid-ali <sajidsyed2021@u.northwestern.edu>
2018-11-27 17:13:05 -0600

    deleted:    build/lib/xray_wave_propagators/prop_cython.py 	renamed:    build/lib/xray_wave_propagators/__init__.py -> build/lib/xwp/__init__.py 	renamed:    build/lib/xray_wave_propagators/fft_utils.py -> build/lib/xwp/fft_utils.py 	renamed:    build/lib/xray_wave_propagators/finite_diff_1d.py -> build/lib/xwp/finite_diff_1d.py 	renamed:    build/lib/xray_wave_propagators/propagators_1d.py -> build/lib/xwp/propagators_1d.py 	renamed:    build/lib/xray_wave_propagators/propagators_2d.py -> build/lib/xwp/propagators_2d.py 	deleted:    dist/xray_wave_propagators-0.0.0-py3.6.egg 	new file:   dist/xwp-0.0.0-py3.6.egg 	modified:   setup.py 	deleted:    xray_wave_propagators.egg-info/.ipynb_checkpoints/native_libs-checkpoint.txt 	deleted:    xray_wave_propagators.egg-info/SOURCES.txt 	deleted:    xray_wave_propagators.egg-info/top_level.txt 	deleted:    xray_wave_propagators/.ipynb_checkpoints/__init__-checkpoint.py 	deleted:    xray_wave_propagators/.ipynb_checkpoints/fft_utils-checkpoint.py 	deleted:    xray_wave_propagators/.ipynb_checkpoints/finite_diff_1d-checkpoint.py 	deleted:    xray_wave_propagators/.ipynb_checkpoints/frft-checkpoint.py 	deleted:    xray_wave_propagators/.ipynb_checkpoints/prop1d_cython-checkpoint.pyx 	deleted:    xray_wave_propagators/.ipynb_checkpoints/propagators_1d-checkpoint.py 	deleted:    xray_wave_propagators/.ipynb_checkpoints/propagators_2d-checkpoint.py 	renamed:    xray_wave_propagators.egg-info/PKG-INFO -> xwp.egg-info/PKG-INFO 	new file:   xwp.egg-info/SOURCES.txt 	renamed:    xray_wave_propagators.egg-info/dependency_links.txt -> xwp.egg-info/dependency_links.txt 	renamed:    xray_wave_propagators.egg-info/not-zip-safe -> xwp.egg-info/not-zip-safe 	new file:   xwp.egg-info/top_level.txt 	renamed:    xray_wave_propagators/__init__.py -> xwp/__init__.py 	renamed:    xray_wave_propagators/fft_utils.py -> xwp/fft_utils.py 	renamed:    xray_wave_propagators/finite_diff_1d.py -> xwp/finite_diff_1d.py 	renamed:    xray_wave_propagators/propagators_1d.py -> xwp/propagators_1d.py 	renamed:    xray_wave_propagators/propagators_2d.py -> xwp/propagators_2d.py

--- build/lib/xray_wave_propagators/prop_cython.py
+++
@@ -1,8 +0,0 @@
-import sys,os
-
-pwd = os.getcwd()
-sys.path.append(pwd+"/xray_wave_propagators/cython/binaries")
-
-from prop1d_cython import exact_prop_cython_1d
-
-__all__ = ['exact_prop_cython_1d']
--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 import os,sys
 
 if __name__ == "__main__":
-   setup(name='xray_wave_propagators',
+   setup(name='xwp',
       description='X-ray wave propagation techniques in python/cython',
       url='https://github.com/s-sajid-ali/xray-wave-propagators',
       author='Sajid',
       author_email='sajidsyed2021@u.northwestern.edu',
-      packages=['xray_wave_propagators'],
+      packages=['xwp'],
       zip_safe=False)
