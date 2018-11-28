38b49ef49b2500abece46a35e29cf53b991a3c38
s-sajid-ali <sajidsyed2021@u.northwestern.edu>
2018-11-28 13:27:45 -0600

    modified:   build/lib/xwp/__init__.py 	new file:   build/lib/xwp/exact_1d.py 	new file:   build/lib/xwp/exact_2d.py 	renamed:    xwp/.ipynb_checkpoints/propagators_1d-checkpoint.py -> build/lib/xwp/spectral_1d.py 	renamed:    xwp/.ipynb_checkpoints/propagators_2d-checkpoint.py -> build/lib/xwp/spectral_2d.py 	modified:   dist/xwp-0.0.0-py3.6.egg 	modified:   xwp.egg-info/SOURCES.txt 	new file:   xwp/.ipynb_checkpoints/__init__-checkpoint.py 	new file:   xwp/.ipynb_checkpoints/exact_1d-checkpoint.py 	new file:   xwp/.ipynb_checkpoints/exact_2d-checkpoint.py 	renamed:    xwp/propagators_1d.py -> xwp/.ipynb_checkpoints/spectral_1d-checkpoint.py 	renamed:    xwp/propagators_2d.py -> xwp/.ipynb_checkpoints/spectral_2d-checkpoint.py 	modified:   xwp/__init__.py 	new file:   xwp/exact_1d.py 	new file:   xwp/exact_2d.py 	new file:   xwp/spectral_1d.py 	new file:   xwp/spectral_2d.py

--- build/lib/xwp/__init__.py
+++ build/lib/xwp/__init__.py
@@ -1,4 +1,6 @@
 from .fft_utils import FFT_2d_Obj
 from .finite_diff_1d import finite_diff_1d_free_space, finite_diff_1d_matter
-from .propagators_1d import propTF, prop1FT, propFF, propIR, exact_prop, exact_prop_numba
-from .propagators_2d import propTF, prop1FT, propFF, propIR, exact_prop, exact_prop_numba
+from .spectral_1d import propTF, prop1FT, propFF, propIR
+from .spectral_2d import propTF, prop1FT, propFF, propIR
+from .exact_1d import exact_prop, exact_prop_numba
+from .exact_2d import exact_prop, exact_prop_numba
--- xwp/.ipynb_checkpoints/spectral_1d-checkpoint.py
+++ xwp/.ipynb_checkpoints/spectral_1d-checkpoint.py
@@ -5,9 +5,7 @@ import numpy as np
 __all__ = ['propTF',
            'prop1FT',
            'propFF',
-           'propIR',
-           'exact_prop_numba',
-           'exact_prop']
+           'propIR']
 
 
 '''
@@ -255,42 +253,3 @@ except:
                 #out_wave[i] += ne.evaluate('f*exp((-1j*pi*x*x)/(wavel*z))*exp((-1j*2*pi*x*x1)/(wavel*z))')
         out_wave *= (1/np.sqrt(1j*wavel*z))*step_in
         return
-
-    
-    
-'''
-Exact propagation (accelerated using numba/numexpr) in 1D
-(Note that the function changes the values of out_wave
-instead of returning an array)
-
-
-in_wave   : profile of the beam at the input plane. 
-out_wave  : array to be filled with values of wave at output plane
-L_in      : side length of the support at input plane
-L_out     : side length of the support at output plane
-wavel     : wavelength
-z         : the propogation distance
-'''
-
-
-try:
-    from numba import jit,prange
-    @jit(nopython=True, parallel=True)
-    def exact_prop_numba(in_wave,out_wave,L_in,L_out,wavel,z):
-        pi = np.pi
-        N_in  = in_wave.shape[0]
-        N_out = out_wave.shape[0]
-        in_domain  = np.linspace(-L_in/2,L_in/2,N_in)
-        out_domain = np.linspace(-L_out/2,L_out/2,N_out)
-        step_in = L_in/N_in
-        for i in prange(N_out):
-            for j in range(N_in):
-                x = in_domain[j]
-                f = in_wave[j]
-                x1 = out_domain[i]
-                out_wave[i] += f*np.exp((-1j*pi*x*x)/(wavel*z))*np.exp((-1j*2*pi*x*x1)/(wavel*z))
-                #out_wave[i] += ne.evaluate('f*exp((-1j*pi*x*x)/(wavel*z))*exp((-1j*2*pi*x*x1)/(wavel*z))')
-        out_wave *= (1/np.sqrt(1j*wavel*z))*step_in
-        return
-except:
-    pass
